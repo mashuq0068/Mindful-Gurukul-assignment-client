dab8caa195c43f5f80d508411008bae894dd2a69
mashuq0068 <mashuq0068@gmail.com>
2023-12-31 03:54:42 +0600

    completed sign up

--- src/Pages/SignUpPage/SignUpPage.jsx
+++ src/Pages/SignUpPage/SignUpPage.jsx
@@ -7,24 +7,35 @@ import toast, { Toaster } from "react-hot-toast";
 
 
 import { VscSaveAs } from "react-icons/vsc";
+
+import { useContext, useState } from "react";
+import Select from 'react-select';
 import { AuthContext } from "../../Providers/AuthProvider";
-import { useContext } from "react";
+
 
 
 const SignUp = () => {
+    const [selectedState, setSelectedState] = useState({ value: 'Gujarat', label: 'Gujarat' });
+
+  const handleStateChange = (selectedOption) => {
+    setSelectedState(selectedOption);
+  };
+  
   const {
     createUser,
-    userWithGoogle
+   
      } = useContext(AuthContext)
   const navigate  = useNavigate()
   const handleSubmit = (e) => {
     e.preventDefault()
     const form = e.target 
      const name = form.name.value
-     const photoUrl = form.photoUrl.value
      const email = form.email.value
      const password = form.password.value
-     console.log(name , photoUrl , email , password)
+     const  phone = form.phone.value 
+     const gender = form.gender.value
+     const selectedSources = Array.from(form.querySelectorAll('input[name="source[]"]:checked')).map(checkbox => checkbox.value);
+     console.log(name  , email , password , phone ,gender , selectedSources , selectedState)
      if(password.length < 6) {
       return toast.error( " password should be more than 6 characters")
        
@@ -58,10 +69,10 @@ const SignUp = () => {
 
                   updateProfile(auth.currentUser, {
                     displayName: name,
-                    photoURL: photoUrl
                 })
                     .then(() => {
                         console.log("profile updated")
+                        toast.success("You signed up successfully and your details has been saved")
                     })
                     .catch(errorData => {
                        
@@ -75,7 +86,8 @@ const SignUp = () => {
               
                 
                     
-                 navigate('/signIn') 
+                 navigate('/login') 
+                 toast.success("You signed up successfully and your details has been saved")
                    
             })
             .catch(error => {
@@ -90,17 +102,8 @@ const SignUp = () => {
             
 
            
-          }
-          const handleGoogle = () => {
-            userWithGoogle()
-                .then(data => {
-                    console.log(data)
-                    navigate('/')
-                })
-                .catch(error => {
-                    
-                    console.error(error.code)
-                })
+          
+          
     
      
   }
@@ -116,16 +119,16 @@ const SignUp = () => {
             <Toaster
                 position="top-right"
                 reverseOrder={false}
-                toastOptions={{className:"2xl:text-2xl text-center"}}
+                toastOptions={{className:" text-center"}}
                 
             />
             <h1 className=" text-4xl text-gradient font-bold text-center mt-8 mb-12 ">Sign<span className=""> Up</span></h1>
-            <div className="w-[80%]  lg:w-[35%] mx-auto">
+            <div className="w-[90%]  lg:w-[45%] mx-auto">
            
             <div className="hero w-full  bg-base-200">
   <div className=" w-full flex-col ">
   
-    <div className=" flex-shrink-0 w-full  h-auto shadow-2xl bg-base-100">
+    <div className=" flex-shrink-0 w-full  h-auto shadow-black shadow-lg bg-base-100">
         
       <form onSubmit={handleSubmit} className="card-body w-full h-auto">
       <div className="form-control">
@@ -155,6 +158,79 @@ const SignUp = () => {
           <input type="number" placeholder="Phone Number" name="phone" className="input  input-bordered" required />
           
         </div>
+        <div className="form-control">
+  <label className="label">
+    <span className="label-text mb-3">Gender</span>
+  </label>
+  <div className="flex gap-2">
+    <input type="radio" name="gender" value="male" id="male" className="radio radio-primary" />
+    <label htmlFor="male" className="2xl:text-[17px] text-[14px]">Male</label>
+
+    <input type="radio" name="gender" value="female" id="female" className="radio radio-primary" />
+    <label htmlFor="female" className="2xl:text-[17px] text-[14px]">Female</label>
+
+    <input type="radio" name="gender" value="others" id="others" className="radio radio-primary" />
+    <label htmlFor="others" className="2xl:text-[17px] text-[14px]">Others</label>
+  </div>
+</div>
+
+<div className="form-control mb-4 mt-4">
+  <label className="label">
+    <span className="label-text mb-3">How did you hear about this?</span>
+  </label>
+  <div className="flex items-start flex-col  gap-4">
+   <div className="flex justify-center items-center gap-1">
+   <input type="checkbox" name="source[]" value="LinkedIn" id="linkedin" className="checkbox  checkbox-primary" />
+    <label className=" 2xl:text-[17px] text-[14px]" htmlFor="linkedin">LinkedIn</label>
+   </div>
+
+    <div className="flex justify-center items-center gap-1">
+    <input type="checkbox" name="source[]" value="Friends" id="friends" className="checkbox checkbox-primary" />
+    <label htmlFor="friends" className="2xl:text-[17px] text-[14px]">Friends</label>
+    </div>
+
+   <div className="flex justify-center items-center gap-1">
+   <input type="checkbox" name="source[]" value="JobPortal" id="jobportal" className="checkbox checkbox-primary" />
+    <label htmlFor="jobportal" className="2xl:text-[17px] text-[14px]" >Job Portal</label>
+   </div>
+
+   <div className="flex justify-center items-center gap-1">
+   <input type="checkbox" name="source[]" value="Others" id="othersource" className="checkbox checkbox-primary" />
+    <label htmlFor="othersource" className="2xl:text-[17px] text-[14px]">Others</label> 
+   </div>
+  </div>
+</div>
+
+<div className="form-control">
+  <label className="label">
+    <span className="label-text">City</span>
+  </label>
+  <select name="city" className="select 2xl:text-[17px] text-[14px] select-bordered">
+    <option value="Mumbai">Mumbai</option>
+    <option value="Pune">Pune</option>
+    <option value="Ahmedabad">Ahmedabad</option>
+  </select>
+</div>
+
+<div className="form-control">
+  <label className="label">
+    <span className="label-text text-base">State</span>
+  </label>
+  <Select
+   value={selectedState}
+   onChange={handleStateChange}
+   defaultValue={"Gujarat"}
+  className=" 2xl:text-[17px] text-[14px]"
+    options={[
+      { value: 'Gujarat', label: 'Gujarat' },
+      { value: 'Maharashtra', label: 'Maharashtra' },
+      { value: 'Karnataka', label: 'Karnataka' },
+    ]}
+    name="state" 
+   />
+ 
+</div>
+
         <div>
           <p className="text-black mt-3  text-center ">Already a user ? go for <Link to='/login' className="text-gradient font-bold ">login</Link></p>
         </div>
